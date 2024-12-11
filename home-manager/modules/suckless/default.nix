a537330bf168f30c9dce8af3a89a65b26ee476a4
world <hello>
2024-12-10 19:07:42 -0500

    tweaked

--- home-manager/modules/suckless/default.nix
+++ home-manager/modules/suckless/default.nix
@@ -1,31 +1,23 @@
 { config, pkgs, dwl-source, dwlb-source, slstatus-source, ... }:
 
-# nixpkgs.overlays = [
-#   (self: super: {
-#     dwl = super.callPackage ./pkgs/dwl-v07.nix { } .overrideAttrs (oldAttrs: {
-#       src = inputs.dwl-source;
-#     });
-#   })
-# ];
-
 let
-  suckless-overlays = [
-    (final: prev: {
-      dwl = (prev.callPackage ./dwl/package.nix { }).overrideAttrs (oldAttrs: {
-        src = dwl-source;
-      });
+  suckless-overlay = final: prev: {
+    
+    dwl = (prev.callPackage ./dwl/package.nix { }).overrideAttrs (oldAttrs: {
+      src = dwl-source;
     });
-    (final: prev: {
-      dwlb = (prev.callPackage ./dwlb/package.nix { }).overrideAttrs (oldAttrs: {
-        src = dwlb-source;
-      });
+    
+    dwlb = (prev.callPackage ./dwlb/package.nix { }).overrideAttrs (oldAttrs: {
+      src = dwlb-source;
     });
-    (final: prev: {
-      slstatus = (prev.callPackage ./slstatus/package.nix { }).overrideAttrs (oldAttrs: {
-        src = slstatus-source;
-      });
+    
+    slstatus = (prev.callPackage ./slstatus/package.nix { }).overrideAttrs (oldAttrs: {
+      src = slstatus-source;
     });
-  ];
+    
+  };
 in {
-  nixpkgs.overlays = suckless-overlays;
+  nixpkgs.overlays = [
+    suckless-overlay
+  ];
 }
