c6983ea0dac70a83f7c805fc9195586e360f3274
world <hello>
2024-12-11 02:03:09 -0500

    renosie fr this time

--- home-manager/modules/renoise/default.nix
+++ home-manager/modules/renoise/default.nix
@@ -2,11 +2,26 @@
 
 let
   renoise-src = "/home/sui/opt/renoise/rns_343_linux_x86_64.tar.gz";
+  renoise-nix = ./renoise-v344.nix;
+  
   result = 
     if builtins.pathExists renoise-src
-    then import ./renoise-full { inherit renoise-src; }
+    then {
+      home.packages = with pkgs; [
+        
+        # renoise-full
+        ((pkgs.callPackage renoise-nix {})
+          .override( { releasePath = renoise-src; } ))
+        
+        rubberband
+        mpg123
+      ];
+      
+      nixpkgs.config.allowUnfreePredicate = pkg: builtins.elem (lib.getName pkg) [
+        "renoise"
+      ];
+    }
     else {};
-in {
-  inherit result;
-}
+  
+in result
   
