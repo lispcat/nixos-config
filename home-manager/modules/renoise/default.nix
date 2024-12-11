ac2e87e238c143155f28957bbc103572438abbb4
world <hello>
2024-12-11 00:24:42 -0500

    wtf

--- home-manager/modules/renoise/default.nix
+++ home-manager/modules/renoise/default.nix
@@ -2,20 +2,24 @@
 
 let
   tar-src = "/home/sui/opt/renoise/rns_343_linux_x86_64.tar.gz";
-in {
-  
-  if ! builtins.pathExists tar-src then
-    null
-  else {
-    
-    home.packages = with pkgs; [
-      ((pkgs.renoise
-        .override( { releasePath = tar-src; } ))
-      )];
-    
-    nixpkgs.config.allowUnfreePredicate = pkg: builtins.elem (lib.getName pkg) [
-      "renoise"
-    ];
-    
-  };
-}
+
+  result =
+    if builtins.pathExists tar-src then
+      let
+        my-renoise =
+          ((pkgs.renoise
+            .override( {releasePath = tar-src;} )))
+      in {            
+        home.packages = with pkgs; [
+          
+        ];
+        
+        nixpkgs.config.allowUnfreePredicate = pkg: builtins.elem (lib.getName pkg) [
+          "renoise"
+        ];
+      };
+    else
+      null;
+in
+result
+
