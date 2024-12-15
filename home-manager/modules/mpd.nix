7c49f5e9b63942ab78c617ef0610694becabbe1a
world <hello>
2024-12-15 03:29:21 -0500

    yayy

--- home-manager/modules/mpd.nix
+++ home-manager/modules/mpd.nix
@@ -1,14 +1,12 @@
-{ config, user, ... }:
+{ config, user, pkgs, ... }:
 
 {
+  # Note, run 'systemctl --user start mpd.service'
   services.mpd = {
     enable = true;
     # considering changing config.xdg.userDirs.music directly? or maybe not?
     musicDirectory = "${config.home.homeDirectory}/Music/library";
     extraConfig = ''
-      # music dir
-      music_directory   "~/Music/library"
-
       # prevent mpd from suddenly resuming
       restore_paused    "yes"
 
