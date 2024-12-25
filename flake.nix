d57f06c2dd96f2f10d27129ace0249fdb4716f3d
meow <woof>
2024-12-25 03:22:27 -0500

    firejailed renoise test

--- flake.nix
+++ flake.nix
@@ -1,46 +1,38 @@
 {
-  description = "A very basic flake";
+  description = "My nix flake";
 
   ## Inputs:
 
   inputs = {
-    # nixpkgs
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
     nixpkgs-stable.url = "github:nixos/nixpkgs/nixos-24.05";
     
-    # home-manager
     home-manager = {
       url = "github:nix-community/home-manager";
       inputs.nixpkgs.follows = "nixpkgs";
     };
-    
-    # suckless repos (hardcoded paths)
-    # dwl-source = {
-    #   url = "/home/sui/Src/dwl-fork";
-    #   flake = false;
-    # };
-    # dwlb-source = {
-    #   url = "/home/sui/Src/dwlb-fork";
-    #   flake = false;
-    # };
-    # slstatus-source = {
-    #   url = "/home/sui/Src/slstatus-fork";
-    #   flake = false;
-    # };
+
+    renoise-source = {
+      path = "/home/sui/opt/rns/rns_344_linux_x86_64.tar.gz";
+      flake = false;
+    }
   };
 
   ## Outputs:
 
-  outputs = inputs@{ self, nixpkgs, nixpkgs-stable, home-manager, ... }:
+  outputs = inputs@{ self, nixpkgs, nixpkgs-stable, home-manager, renoise-source, ... }:
     let
-      system      = "x86_64-linux";
-      user        = "sui";
+      user   = "sui";
+      system = "x86_64-linux";
+      
       pkgs        = import nixpkgs        { inherit system; };
       pkgs-stable = import nixpkgs-stable { inherit system; };
     in {
       nixosConfigurations.NixOwOs = nixpkgs.lib.nixosSystem
         {
-          specialArgs = { inherit system inputs user; };
+          specialArgs = {
+            inherit system inputs user renoise-source;
+          };
           modules = [
             ./unfree-merger.nix
             ./nixos/configuration.nix
--- nixos/modules/desktop/firejail/default.nix
+++ nixos/modules/desktop/firejail/default.nix
@@ -4,6 +4,8 @@
   programs.firejail.enable = true;
   
   imports = [
+    ./renoise
+    
     ./foss.nix
     ./games.nix
     ./proprietary.nix
