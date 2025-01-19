3aa42213c917e7259051f8630f2301420dd6a2c5
meow <woof>
2025-01-19 05:00:07 -0500

    hope this works

--- home-manager/modules/input/default.nix
+++ home-manager/modules/input/default.nix
@@ -1,12 +1,15 @@
-{ pkgs, ... }:
+{ pkgs, config, ... }:
 
 {
   i18n.inputMethod = {
     enabled = "fcitx5";
     fcitx5.addons = with pkgs; [
-      fcitx5-mocz
+      fcitx5-mozc
       fcitx5-gtk
       fcitx5-material-color
     ];
   };
+
+  home.file.".config/fcitx5/conf/classicui.conf".source =
+    ./classicui.conf;
 }
